2009d9919e010ef5134767195b85e4c41194a648
Godswill Azubike <43970405+Godswill-Azubike@users.noreply.github.com>
2022-11-08 11:50:53 +0100

    chore: add contact widget

--- lib/widget/home_widget.dart
+++ lib/widget/home_widget.dart
@@ -6,9 +6,39 @@ class HomeWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const SafeArea(
-      child: Center(
-        child: Text('home'),
-      ),
+      child: ContactList(),
     );
+  }
+}
+
+class ContactList extends StatelessWidget {
+  const ContactList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 10,
+      itemBuilder: (context, index){
+        return ListTile(
+          leading: Image.asset("assets/images/add-friend.png"),
+          title: const Text("Joseph Okoro", 
+          style: TextStyle(fontSize: 20.0, 
+          fontWeight: FontWeight.w700, 
+          color: Colors.blue,),
+          ),
+          subtitle: const Text("+234-903-0297-400"),
+          trailing: ElevatedButton.icon(
+            onPressed: (){}, 
+            icon: const Icon(Icons.add), 
+            label: const Text("Add"),
+          ),
+          // trailing: IconButton(
+          //   onPressed: (){}, 
+          //   icon: const Icon(Icons.add),
+            
+          // )
+        );
+      }
+      );
   }
 }
